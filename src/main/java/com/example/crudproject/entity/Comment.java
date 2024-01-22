5df43590451aec64de009c44e37a05921d3d55e5
“mafatofu” <qwqw0310@gmail.com>
2024-01-22 17:54:29 +0900

    240122 entity update (article/board/comment/user)

---
+++ src/main/java/com/example/crudproject/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.example.crudproject.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Setter
+    private String content;
+    @Setter
+    @ManyToOne
+    private Article article;
+
+    @ManyToOne
+    private User user;
+
+    public Comment(String content, Article article,User user) {
+        this.content = content;
+        this.article = article;
+        this.user = user;
+    }
+}
