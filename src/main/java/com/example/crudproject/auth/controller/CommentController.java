dc7153572b20fb4f640673a5668c03769e6b307c
teleport1005 <rabbinet@naver.com>
2024-01-30 16:40:06 +0900

    REST Url.md 추가

--- src/main/java/com/example/crudproject/auth/controller/CommentController.java
+++ src/main/java/com/example/crudproject/auth/controller/CommentController.java
@@ -7,18 +7,20 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/comments")
+@RequestMapping("/article/{articleId}")
 public class CommentController {
 
     private final CommentService commentService;
 
 
-
+    // TODO @RequestMapping ("/article/{articleId}") 추가하여 수정 부분 있습니다
     @PostMapping
     public CommentDto createComment(
             @RequestBody CommentDto dto,
             @RequestParam("userId") Long userId,
-            @RequestParam("articleId") Long articleId) {
+            @PathVariable("articleId") Long articleId
+    )
+            {
         return commentService.createComment(articleId, userId, dto);
     }
 }
